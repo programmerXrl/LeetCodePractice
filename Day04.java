92d12724092a4b27ca419f219214fc188ea76583
programergo <102668048+programergo@users.noreply.github.com>
2022-10-12 18:53:37 +0800

    2022/10/12

--- Day04.java
+++ Day04.java
@@ -1,22 +1,21 @@
-package 比特笔试强训;
+package Level1;
 
 public class Day04 {
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(1);
-        ListNode l2 = new ListNode(2);
-        ListNode l3 = new ListNode(4);
-        l1.next=l2;
-        l2.next=l3;
-
-        ListNode l4 = new ListNode(1);
-        ListNode l5 = new ListNode(3);
-        ListNode l6 = new ListNode(4);
-        l4.next=l5;
-        l5.next=l6;
-        Day04 day04 = new Day04();
-        day04.PrintListNode(day04.mergeTwoLists(l1,l4));
+        Day04 tool = new Day04();
+        ListNode h1 = new ListNode(3);
+        ListNode h2 = new ListNode(2);
+        ListNode h3 = new ListNode(0);
+        ListNode h4 = new ListNode(-4);
+        ListNode h5 = new ListNode(5);
+        ListNode h6 = new ListNode(6);
+        h1.next=h2;
+        h2.next=h3;
+        h3.next=h4;
+        h4.next=h2;
+        System.out.println(tool.detectCycle(h1).val);
     }
-    public void PrintListNode(ListNode head){
+    public void printList(ListNode head){
         ListNode cur = head;
         while (cur!=null){
             System.out.print(cur.val+"->");
@@ -24,55 +23,36 @@ public class Day04 {
         }
         System.out.print("null");
     }
-    public ListNode mergeKLists(ListNode[] lists) {
-        ListNode ans = null;
-        for (int i = 0; i < lists.length; i++) {
-            ans=mergeTwoLists(ans,lists[i]);
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
         }
-        return ans;
+        return slow;
     }
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1==null && l2==null){
-            return null;
-        }
-        if (l1==null){
-            return l2;
-        }
-        if (l2==null){
-            return l1;
-        }
-        //l1 l2 都存在
-        ListNode head = new ListNode();
-        ListNode headNode = head;
-        ListNode cur1 = l1;
-        ListNode cur2 = l2;
-        while (cur1!=null && cur2!=null){
-            if (cur1.val<=cur2.val){
-                headNode.next=cur1;
-                cur1=cur1.next;
-                headNode=headNode.next;
+    public ListNode detectCycle(ListNode head) {
+        if(head==null) return null;
+        if (head.next==null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast!=null){
+            slow = slow.next;
+            if (fast.next!=null){
+                fast=fast.next.next;
             }else {
-                headNode.next=cur2;
-                cur2=cur2.next;
-                headNode=headNode.next;
+                return null;
             }
-        }//跳出循环 L1 或 L2 一个为空
-        headNode.next=cur1==null?cur2:cur1;
-        return head.next;
-    }
-    public ListNode reverseList(ListNode head) {
-        if (head==null){
-            return null;
+            if (fast==slow){
+                ListNode ptr = head;
+                while (ptr!=slow){
+                    ptr=ptr.next;
+                    slow=slow.next;
+                }
+                return ptr;
+            }
         }
-        ListNode cur = head;
-        ListNode pre = null;
-        ListNode tmp = null;
-        while (cur!=null){
-            tmp=cur.next;
-            cur.next=pre;
-            pre=cur;
-            cur=tmp;
-        }
-        return pre;
+        return null;
     }
 }
