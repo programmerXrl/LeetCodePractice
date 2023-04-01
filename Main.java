9cb68fa61fa79d86b0928b25b11997ede612a0d9
programmerXrl <102668048+programmerXrl@users.noreply.github.com>
2023-04-01 21:38:51 +0800

    迷宫问题

--- Main.java
+++ Main.java
@@ -1,44 +1,68 @@
-package 算法题;
+package month3;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        String[] strings = new String[n];
-        for (int i = 0; i < n; i++) {
-            strings[i]=scanner.nextLine();
-        }
-        boolean isLex = true;
-        boolean isLen = true;
-        for (int i = 1; i < n; i++) {
-            if (isLen && !isLengths(strings[i-1],strings[i])){
-                isLen=false;
-            }
-            if (isLex && !isLexicographically(strings[i-1],strings[i])){
-                isLex=false;
-            }
-        }
-        if (isLex && isLen){
-            System.out.println("both");
-        }else if (isLen){
-            System.out.println("lengths");
-        }else if (isLex){
-            System.out.println("Lexicographically");
-        }else {
-            System.out.println("none");
-        }
-    }
-    public static boolean isLexicographically(String s1 , String s2){
-        if (s2.contains(s1)) return true;
-        for (int i = 0; i < s1.length(); i++) {
-            if (i>=s2.length()) return false;
-            if (s1.charAt(i)>s2.charAt(i)) return false;
-        }
-        return true;
-    }
-    public static boolean isLengths(String s1 , String s2){
-        return s1.length() < s2.length();
+class Node{
+    int x;
+    int y;
+    public Node(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 }
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        String[] arr = str.split(" ");
+        int row = Integer.parseInt(arr[0]);
+        int col = Integer.parseInt(arr[1]);
+        int[][] book = new int[row][col];
+        int[][] mat = new int[row][col];
+        ArrayList<Node> path = new ArrayList<>();
+        ArrayList<Node> minPath = new ArrayList<>();
+        for (int i = 0; i < row; i++) {
+            str = reader.readLine();
+            arr = str.split(" ");
+            for (int j = 0; j < col; j++) {
+                mat[i][j] = Integer.parseInt(arr[j]);
+            }
+        }
+        getMinPath(mat,row,col,0,0,book,path,minPath);
+        for (Node node : minPath){
+            System.out.println("(" + node.x + "," + node.y + ")");
+        }
+    }
+    static void getMinPath(int[][] mat, int row, int col, int x, int y, int[][] book, ArrayList<Node> path, ArrayList<Node> minPath){
+        //判断当前位置能不能走
+        if (x < 0 || x >= col || y < 0 || y >= row || book[x][y] == 1 || mat[x][y] == 1){
+            return;
+        }
+        //如果能走
+        path.add(new Node(x,y));
+        book[x][y] = 1;
+        //判断是否到了目的地
+        if (x == row-1 && y == col-1){
+            if (minPath.isEmpty() || path.size() < minPath.size()){
+                minPath.clear();
+                for (Node node : path){
+                    minPath.add(node);
+                }
+            }
+        }
+        //上下左右走
+        getMinPath(mat,row,col,x,y-1,book,path,minPath);
+        getMinPath(mat,row,col,x,y+1,book,path,minPath);
+        getMinPath(mat,row,col,x-1,y,book,path,minPath);
+        getMinPath(mat,row,col,x+1,y,book,path,minPath);
+
+        //如果不能走,就往回退,同时删除标记
+        path.remove(path.size()-1);
+        book[x][y] = 0;
+    }
+}
+
