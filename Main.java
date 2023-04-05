79e44b00ef2f70b4cd9ce975dfc1dc94ac06efcb
programmerXrl <102668048+programmerXrl@users.noreply.github.com>
2023-04-05 16:32:59 +0800

    贪心

--- Main.java
+++ Main.java
@@ -5,34 +5,47 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        String[] arr = str.split(" ");
-        ArrayList<String> arrayList = new ArrayList<>();
-        StringBuilder builder = new StringBuilder();
-        boolean flag = true;
-        for (String s : arr){
-            if (s.charAt(0)!='"' && flag){
-                arrayList.add(s);
-            }else {
-                //遇到了"
-                flag = false;
-                if (s.charAt(s.length()-1)!='"'){
-                    //如果最后一位不是"
-                    builder.append(s.substring(0));
-                    builder.append(" ");//加一个空格
-                }else {
-                    //最后一位是"
-                    builder.append(s.substring(0,s.length()-1));
-                    flag = true;
-                    String copy = builder.toString();
-                    arrayList.add(copy.substring(1));
-                    builder.delete(0,builder.length());
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[m+1];
+        for (int i = 0; i < m+1; i++) {
+            arr[i] = Integer.MAX_VALUE;
+        }
+        arr[n] = 0;
+        for (int i = n; i < m; i++) {
+            if (arr[i] == Integer.MAX_VALUE){
+                //如果当前石板上没有数字,表示到达不了,直接跳过
+                continue;
+            }
+            List<Integer> list = helper(i);
+            for(int j : list){
+                if (i + j <= m && arr[i+j] == Integer.MAX_VALUE){//如果目标跳板上没有数字
+                    arr[i + j] = arr[i] + 1;
+                }else if (i + j <=m){
+                    //如果目标跳板上有标记
+                    //取最小值
+                    arr[i + j] = Math.min(arr[i + j],arr[i] + 1);
                 }
             }
         }
-        System.out.println(arrayList.size());
-        for (String s : arrayList){
-            System.out.println(s);
+        if (arr[m]!=Integer.MAX_VALUE){
+            //有数字
+            System.out.println(arr[m]);
+        }else {
+            System.out.println(-1);
         }
+    }
+    static List<Integer> helper(int num){
+        //求num约数集的函数
+        List<Integer> list = new ArrayList<>();
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0){
+                list.add(i);
+                if (num / i != i){
+                    list.add(num / i);
+                }
+            }
+        }
+        return list;
     }
 }
