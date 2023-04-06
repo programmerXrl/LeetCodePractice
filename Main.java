89b3e8490b7fd451c00fe3d93c7fbc6eff6a07b8
programmerXrl <102668048+programmerXrl@users.noreply.github.com>
2023-04-06 13:15:34 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,45 +5,43 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
-       int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            int a = 0;//大于这个数
-            int b = 0;//小于这个数
-            for (int j = 0; j < n; j++) {
-                if (arr[i] < arr[j]){
-                    a++;
-                }else if(arr[i] > arr[j]){
-                    b++;
-                }
-            }
-            if (a <= b){
-                result[i] = 0;
-                System.out.println(result[i]);
-            }else {
-                int[] tar = new int[n-1-b];
-                int x = 0;
-                for (int j = 0; j < n; j++) {
-                    if (arr[i]<arr[j]){
-                        tar[x] = arr[j];
-                        x++;
-                    }
-                }
-                Arrays.sort(tar);
-                //System.out.println(Arrays.toString(tar));
-                int k = -1;
-                while (a > b){
-                    k++;
-                    a--;
-                    b++;
-                }
-                System.out.println(tar[k]-arr[i]+1);
+      Scanner sc = new Scanner(System.in);
+      int num = sc.nextInt();
+      ArrayList<Integer> arrayList = new ArrayList<>();
+      arrayList.add(2);
+      for(int i=3; i< 1000; i+=2){
+          if (helper(i)){
+              arrayList.add(i);
+          }
+      }
+      int min = Integer.MAX_VALUE;
+      int a = 0;
+      int b = 0;
+      for(int i=0; i<arrayList.size(); i++){
+          for(int j=0; j<arrayList.size(); j++){
+              if (i >= num || j >= num){
+                  continue;
+              }
+              if (arrayList.get(i) + arrayList.get(j) == num){
+                  int x = Math.abs(arrayList.get(i) - arrayList.get(j));
+                  if (x < min){
+                      min = x;
+                      a = arrayList.get(i);
+                      b = arrayList.get(j);
+                  }
+              }
+          }
+      }
+      System.out.println(a);
+      System.out.println(b);
+    }
+    static boolean helper(int num){
+        //判断是不是素数
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if (num % i == 0){
+                return false;
             }
         }
+        return true;
     }
 }
